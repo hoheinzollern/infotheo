acb20a1ce643966ad2b25c2d2311d464324c0f7a
Chang-Rong Xue <changrong.xue@hotmail.com>
2026-01-30 09:19:36 +0100

    temp commit

--- robust/weightedmean_l2_lmodType.v
+++ robust/weightedmean_l2_lmodType.v
@@ -7,9 +7,12 @@ From mathcomp Require Import Rstruct reals mathcomp_extra.
 From mathcomp Require Import mxalgebra.
 From mathcomp Require Import landau.
 From robot Require Import euclidean. 
+From mathcomp Require Import all_ssreflect all_algebra.
+Import GRing.Theory.
 Require Import ssr_ext ssralg_ext bigop_ext realType_ext realType_ln.
 Require Import fdist proba.
 Require coqRE.
+
 
 Require Import Coq.Reals.Reals Lra.
 
@@ -36,17 +39,99 @@ Abort.
 
 End test.
 
+Section expectation.
+Variables (U : finType) (P : R.-fdist U). 
+
+Lemma Ex_add_lmod (V : lmodType R) (X Y : {RV P -> V}) :
+  `E (X `+ Y) = `E X + `E Y.
+Proof.
+rewrite /Ex /add_RV.
+rewrite -big_split.
+by apply: eq_bigr => u _; rewrite scalerDr.
+Qed.
+
+Lemma Ex_opp_lmod (V : lmodType R) (X : {RV P -> V}) :
+  `E (`-- X) = - `E X.
+Proof.
+rewrite /Ex /opp_RV.
+(* rewrite big_morph_oppr /=. *)
+(* by apply: eq_bigr => u _; rewrite scalerN. *)
+Admitted.
+
+Definition sub_RV_lmod (V : lmodType R) (X Y : {RV P -> V}) : {RV P -> V} :=
+  fun u => X u - Y u.
+
+Lemma Ex_sub_lmod (V : lmodType R) (X Y : {RV P -> V}) :
+  `E (sub_RV_lmod X Y) = `E X - `E Y.
+Proof.
+rewrite /sub_RV_lmod.
+(* rewrite (sub_RV_neg X Y). *)
+(* rewrite Ex_add_lmod. *)
+Admitted.
+
+Lemma Ex_const_lmod (V : lmodType R) (m : V) :
+  `E (const_RV P (T := V) m) = m.
+Proof.
+rewrite /Ex /const_RV /=.
+(* rewrite -big_distrl /=. *)
+(* by rewrite FDist.f1 scale1r. *)
+Admitted.
+
+End expectation.
+
+Locate addrA. 
+About addrA.
+
+Search (_ + (_ - _) = _ + _ - _).
+
+Section Expand.
+Variable R : ringType.
+Variables (m n p : nat).
+Variables (A B : 'M[R]_(m, n)) (C D : 'M[R]_(m, n)).
+
+Lemma my_addrB (T : zmodType) (x y z : T) :
+  x + (y - z) = x + y - z.
+Proof. 
+  rewrite addrA. 
+  by [].
+Qed.
+
+Locate "*m".
+
+Lemma expand_transpose_sub_mul :
+  (A^T - B^T) *m (C - D)
+  = A^T *m C - B^T *m C - A^T *m D + B^T *m D.
+Proof.
+  rewrite mulmxBl.
+  rewrite mulmxBr.
+rewrite mulmxBr.        (* Distribute Right again *)
+rewrite opprB.   (* Fixes the sign:  ... + (B^T D - B^T C) ... *)
+rewrite addrA.   (* Removes parens:  ... + B^T D - B^T C ...   *)
+rewrite addrAC.  (* Swaps middle:    ... - B^T C + B^T D ...   *)
+rewrite addrAC.  (* Swaps end:       ... - A^T D + B^T D       *)
+by ring.
+set t1 := A^T *m C.
+set t2 := B^T *m C.
+set t3 := A^T *m D.
+set t4 := B^T *m D.
+Admitted.
+End Expand.
+
+
+
 Section covariance. 
 Local Open Scope ring_scope.
-Let R := Rdefinitions.R. 
+Context {R : realType}.
 
-Variables (U : finType) (d : nat) (P : R.-fdist U) (X : {RV P -> 'rV[R]_d}). 
+Variables (U : finType) (d : nat) (P : R.-fdist U) (X Y: {RV P -> 'rV[R]_d}). 
 
 Check `E X.
 Check `E X ord0.
 (* Check `E ((X `-cst `E X)^T *m (X `-cst `E X)^T). *)
 
-Locate "`-cst".
+Locate "`-cst". 
+
+About "`-cst".
 
 Definition mu : 'rV[R]_d := `E X.
 
@@ -54,17 +139,58 @@ Definition transpose_rv {m n :nat}
   (Y : {RV P -> 'M[R]_(m, n)} ) : {RV P -> 'M[R]_(n, m)}
   := fun u => (Y u)^T.
 
-Check transpose_rv (X `-cst mu).
+Local Notation "X ^TT" := (transpose_rv X).
+
+Check (X `-cst mu)^TT.
+Check Y^TT.
 
 Definition matmul_rv {m n o:nat} 
   (Y : {RV P -> 'M[R]_(m, o)}) (Z : {RV P -> 'M[R]_(o, n)}) 
   :{RV P -> 'M[R]_(m, n)} := 
   fun u => (Y u) *m (Z u).
 
+Local Notation "Y *M Z" := (matmul_rv Y Z) (at level 40, left associativity).
 
+Check (X `-cst mu)^TT *M (Y `-cst `E Y).
+
+
+(* Cov[X, Y] = E[(X - E[X]) (Y - E[Y])^T] *)
 Definition Cov : 'M[R]_(d, d) :=
   (* transpose cannot apply on matrix/vector of "RV P -> 'rV[R]_d" *) 
-  `E (matmul_rv (transpose_rv (X `-cst mu)) (X `-cst mu)).
+  `E ((X `-cst `E X)^TT *M (Y `-cst `E Y)). 
+
+(* 
+\mathrm{Cov}[X, Y] = \mathbb{E}[X^T Y] - \mathbb{E}[X] \mathbb{E}[Y]^T
+*)
+
+Lemma trmxB {m n} (A B : 'M[R]_(m, n)) : (A - B)^T = A^T - B^T.
+Proof. by apply/matrixP => i j; rewrite !mxE. Qed.
+
+Lemma mulmxBl {m n p} (A B : 'M[R]_(m, n)) (C : 'M[R]_(n, p)) :
+  (A - B) *m C = A *m C - B *m C.
+Proof. by rewrite mulmxDl mulNmx. Qed.
+
+Lemma mulmxBr {m n p} (A : 'M[R]_(m, n)) (B C : 'M[R]_(n, p)) :
+  A *m (B - C) = A *m B - A *m C.
+Proof. by rewrite mulmxDr mulmxN. Qed.
+
+Lemma mat_opp_mix_transpose 
+  {m n} (A: {RV P -> 'M[R]_(m, n)}) (B : 'M[R]_(m, n)) :
+  (A `-cst B)^TT = A^TT `-cst B^T. 
+Proof.
+Admitted.
+
+
+Check mulmxDr.
+
+
+Lemma Cov_Ex:  
+  Cov = (`E (X^TT *M Y))- (`E X)^T *m (`E Y).
+Proof.
+rewrite /Cov.
+rewrite mat_opp_mix_transpose.
+Qed.
+
 
 Check eigenvalue.
 Print eigenvalue.
@@ -142,6 +268,7 @@ Definition stableT_rv (mu: 'rV[R]_d)  :=
 && ( `| Ex Q (fun u => ((v *d (X u - mu)))^+2 : R^o)- 1| 
   <= delta^+2 / eps ).
 
+(*we might need definition of a distribution is stable *)
 
 Lemma stableT_eq : forall mu, @stableT mu <-> @stableT_rv mu.
 Admitted. 
@@ -231,21 +358,35 @@ End subtractive_contamination.
 (* ```                                                                        *)
 (*        {O_F f} == the type of functions that are a bigO of f near F        *)
 
+
+Section law_of_total_covariance.
+
+
+(* Law of total covariance *)
+End law_of_total_covariance.
+
 (*
 Fromalize Lemma 2.7 in Algorithmic High-Dimensional Robust Statistics
 *)
 Section Certificate_for_Empirical_Mean. 
 Local Open Scope ring_scope.
-Let R := Rdefinitions.R.
-Variables (d: nat) (U : finType) (P : {fdist U}) (V : lmodType R) 
-  (X Y : {RV P -> 'rV[R]_d}) (C : {ffun U -> R})
+Context {R : realType}.
+Variables (d: nat) (U : finType) (P : R.-fdist U) (V : lmodType R) 
+  (X Y E: {RV P -> 'rV[R]_d}) 
+  (Good : {set U})  (* good set *)
+  (bad : {set U})  (* bad set *)
+  (C : {ffun U -> R})
+
   (mu: 'rV[R]_d)
+(* What is S here `b`b*)
   (S : {set U}) (eps delta lambda: R).  
+
 Hypothesis eps0: 0 < eps. 
 Hypothesis delta_ge_eps : delta >= eps. 
 Hypothesis lambda0: 0 <= lambda.
 (* Hypothesis X_stable_mu: *)
 Hypothesis lambda_eigen: forall a, eigenvalue (Cov Y) a -> a <= 1 + lambda.
+Hypothesis S_eps: Pr P S = eps.
 
 
 (* Ind in proba.v *) 
@@ -256,17 +397,20 @@ Hypothesis lambda_eigen: forall a, eigenvalue (Cov Y) a -> a <= 1 + lambda.
   - go through the proof of Lemma 2.7 in the paper and formalize each step.
   - Big O notation (follow up Dianonicolas)
 we need 
-  - use indicator function to define Y 
+  - use indicator function to define Y  （Proba.v section Ind.)
   - definition of conditional covariance
   - lemma, totoal covariance and total expectation 
 *)
 
 
 
+
 Lemma Certificate_for_Empirical_Mean : 
   exists C, mx_norm (`E Y - mu) <= C * (delta + Num.sqrt (eps * lambda)).
 Proof.
-pose I : {RV P -> R } := Ind S. 
+pose I : {RV P -> R } := Ind S. (* move to let *)
+
+Check I.
 Check `Pr[I = 0]:R.
 Check Pr P S:R.
 Check Pr P S = eps.
@@ -296,6 +440,3 @@ Abort.
 
 
 End Certificate_for_Empirical_Mean.
-
-
-
